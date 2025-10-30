ea8507978f1e25a71fefba7ac01474e4d35c9afa
Salypse <84153483+Salypse@users.noreply.github.com>
2025-10-30 11:15:49 -0500

    Create Next and Previous image buttons that correctly cycle through the given image list

--- image-carousel.js
+++ image-carousel.js
@@ -15,15 +15,49 @@ const imageCarouselDisplay = function(div, imageList) {
     const carouselButtons = document.createElement("div")
     carouselButtons.classList.add("carousel-buttons")
 
+    const previousImageButton = document.createElement("button")
+    previousImageButton.classList.add("previous-image")
+    previousImageButton.textContent = "<-"
+    previousImageButton.addEventListener("click", () => {
+        displayPreviousImage(findCurrentImageIndex())
+    })
+
+    const nextImageButton = document.createElement("button")
+    nextImageButton.classList.add("next-image")
+    nextImageButton.textContent = "->"
+    nextImageButton.addEventListener("click", () => {
+        displayNextImage(findCurrentImageIndex())
+    })
+
+    carouselButtons.append(
+        previousImageButton,
+        nextImageButton,
+    )
+
     currrentImageCarousel.append(pictureFrame,carouselButtons)
+
+    
+    function displayNextImage(startIndex) {
+        startIndex = (startIndex === imageList.length - 1) ? 0 : startIndex + 1
+        pictureFrame.src = imageList[startIndex]
+    }
+
+    function displayPreviousImage(startIndex) {
+        startIndex = (startIndex === 0 ) ? (imageList.length - 1) : startIndex - 1
+        pictureFrame.src = imageList[startIndex]
+    }
+
+    function findCurrentImageIndex() {
+        return imageList.findIndex(image => pictureFrame.src.endsWith(image))
+    }
 }
 
 const imageSrcList = [
-    "./test-images/now-you-see-me-now-you-dont(2025).jpg",
-    "./test-images/predator-badlands(2025).jpg",
-    "./test-images/the-running-man(2025).jpg",
-    "./test-images/wake-up-dead-man(2025).jpg",
-    "./test-images/zootopia-2(2025).jpg"
+    "/home/salypse/repos/image-carousel/test-images/now-you-see-me-now-you-dont(2025).jpg",
+    "/home/salypse/repos/image-carousel/test-images/predator-badlands(2025).jpg",
+    "/home/salypse/repos/image-carousel/test-images/the-running-man(2025).jpg",
+    "/home/salypse/repos/image-carousel/test-images/wake-up-dead-man(2025).jpg",
+    "/home/salypse/repos/image-carousel/test-images/zootopia-2(2025).jpg",
 ]
 
 
